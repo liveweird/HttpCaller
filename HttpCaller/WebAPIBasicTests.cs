d8fd92dd7675fde05e77484055ca09dc0cb0240a
Sebastian Gebski <sebastian@gebski.net>
2015-10-20 12:39:22 +0200

    WebAPI client

---
+++ HttpCaller/WebAPIBasicTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace HttpCaller
+{
+    [TestClass]
+    public class WebApiBasicTests
+    {
+        private readonly string _serviceUri = "http://localhost:4000";
+
+        [TestMethod]
+        public void NoOneIsListening()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(_serviceUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                Should.Throw<HttpRequestException>(async () =>
+                                                   {
+                                                       await client.GetAsync("api/anybody/home");
+                                                   });
+            }
+        }
+
+        [TestMethod]
+        public void SimpleGet()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(_serviceUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                /*
+                var responseTask = client.GetAsync("api/products/1");
+                responseTask.Wait();
+                if (response.IsSuccessStatusCode)
+                {
+                    Product product = await response.Content.ReadAsAsync<Product>();
+                    Console.WriteLine("{0}\t${1}\t{2}", product.Name, product.Price, product.Category);
+                }
+                */
+            }
+        }
+    }
+}
